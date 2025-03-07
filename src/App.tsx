06c2fb5e2029c28ccd0bade07b81bbe868225df0
YeThura-424 <rathanpoudle.26@gmail.com>
2025-03-07 21:19:28 +0630

    install router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
 import "./globals.css";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes  */}
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-up" element={<SignupForm />} />
+
+        {/* private routes  */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
   );
 };
 
