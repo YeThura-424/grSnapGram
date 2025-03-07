035d1d0cd63e5a8ab406d8f408f1f2d968096d2e
YeThura-424 <rathanpoudle.26@gmail.com>
2025-03-07 21:58:59 +0630

    update

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 const SignupForm = () => {
-  return <div>SignupForm</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
+  );
 };
 
 export default SignupForm;
