d4e29e23a989bb59370aba27ad75bc0effa985ce
YeThura-424 <rathanpoudle.26@gmail.com>
2025-03-09 22:01:59 +0630

    auth user from signup function

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -15,6 +15,7 @@ import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
 import Loader from "@/components/shared/Loader";
 import { Link } from "react-router-dom";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
   const isLoading = false;
@@ -29,10 +30,10 @@ const SignupForm = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    const userAccount = await createUserAccount(values);
+
+    console.log(userAccount);
   }
 
   return (
@@ -110,7 +111,7 @@ const SignupForm = () => {
               name="password"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Name</FormLabel>
+                  <FormLabel>Passwrod</FormLabel>
                   <FormControl>
                     <Input
                       type="password"
