b91dc20bd393d76da6284617abf44993fe10a90f
YeThura-424 <rathanpoudle.26@gmail.com>
2025-03-14 22:01:50 +0630

    update sign in account api

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -53,7 +53,10 @@ export async function saveUserToDB(user: {
 
 export async function signInAccount(user: { email: string; password: string }) {
   try {
-    const session = await account.createSession(user.email, user.password);
+    const session = await account.createEmailPasswordSession(
+      user.email,
+      user.password
+    );
 
     return session;
   } catch (error) {
