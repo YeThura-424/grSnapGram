0357134fdf7bb7486465e08cb422e4d0c8d0685f
YeThura-424 <rathanpoudle.26@gmail.com>
2025-03-18 21:04:19 +0630

    post detail

--- src/_root/pages/index.ts
+++ src/_root/pages/index.ts
@@ -2,3 +2,4 @@ export { default as Home } from "./Home";
 export { default as Explore } from "./Explore";
 export { default as CreatePost } from "./CreatePost";
 export { default as EditPost } from './EditPost'
+export { default as PostDetail } from './PostDetail'
