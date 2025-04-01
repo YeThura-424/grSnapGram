c2bb8e3bda7abd47766a19292394bb822ae6a5b0
YeThura-424 <rathanpoudle.26@gmail.com>
2025-04-01 21:04:22 +0630

    add:comment icon on post stats component

--- src/components/shared/PostStats.tsx
+++ src/components/shared/PostStats.tsx
@@ -75,20 +75,34 @@ const PostStats = ({ post, userId }: PostStatsProps) => {
     <div
       className={`flex justify-between items-center z-20 ${containerStyles}`}
     >
+
       <div className="flex gap-2 mr-5">
+        {likes.length > 0 && (
+          <div className="flex gap-2 ">
+            <img
+              src={`${checkIsLiked(likes, userId)
+                ? "/assets/icons/liked.svg"
+                : "/assets/icons/like.svg"
+                }`}
+              alt="like"
+              width={20}
+              height={20}
+              onClick={(e) => handleLikePost(e)}
+              className="cursor-pointer"
+            />
+            <p className="small-medium lg:base-medium">{likes.length}</p>
+          </div>
+        )}
+
+
         <img
-          src={`${
-            checkIsLiked(likes, userId)
-              ? "/assets/icons/liked.svg"
-              : "/assets/icons/like.svg"
-          }`}
+          src="/assets/icons/comment.svg"
           alt="like"
           width={20}
           height={20}
-          onClick={(e) => handleLikePost(e)}
           className="cursor-pointer"
         />
-        <p className="small-medium lg:base-medium">{likes.length}</p>
+        <p className="small-medium lg:base-medium">6</p>
       </div>
 
       <div className="flex gap-2">
